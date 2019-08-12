854076f2a80951e80e0e9dac5e61ff82e5bcfd70
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-12 16:06:36 +0200

    Adding some items to the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg'),
+    new Recipe('Another recipe', 'another tasty one', 'https://www.publicdomainpictures.net/pictures/300000/nahled/sushi-items-on-a-platter.jpg')
+  ];
 
   constructor() { }
 
