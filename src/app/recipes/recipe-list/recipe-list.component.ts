752edec27f048f682cfa1563981e85fa0a9fc4f5
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-13 14:44:00 +0200

    Link the selected recipe with the actual details component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -8,9 +8,13 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test', 'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg'),
-    new Recipe('Another recipe', 'another tasty one', 'https://www.publicdomainpictures.net/pictures/300000/nahled/sushi-items-on-a-platter.jpg')
+    new Recipe('A test recipe', 'this is a test',
+      'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg'),
+    new Recipe('Another recipe', 'another tasty one',
+      'https://www.publicdomainpictures.net/pictures/300000/nahled/sushi-items-on-a-platter.jpg')
   ];
 
   constructor() { }
@@ -18,4 +22,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
