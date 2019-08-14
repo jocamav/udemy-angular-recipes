f8942a35e0ba3570300fd0eab7de9dcc10f9c0b3
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-14 14:43:05 +0200

    Use service to select the recipes

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -14,6 +14,7 @@ export class RecipesComponent implements OnInit {
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(selectedRecipe => this.selectedRecipe = selectedRecipe);
   }
 
 }
