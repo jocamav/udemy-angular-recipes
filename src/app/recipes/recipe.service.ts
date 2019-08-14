3bb06726c735264938612ec25505c3f73423c3ef
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-14 15:14:59 +0200

    Adding ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient-model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,19 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test',
-      'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg'),
-    new Recipe('Another recipe', 'another tasty one',
-      'https://www.publicdomainpictures.net/pictures/300000/nahled/sushi-items-on-a-platter.jpg')
+    new Recipe('Miniburger', 'Some mini burgers',
+      'https://www.maxpixel.net/static/photo/1x/Burgers-Burger-Chickpeas-Recipes-Food-Vegetables-2920072.jpg',
+      [
+        new Ingredient('Meat', 5),
+        new Ingredient('Bread', 3),
+        new Ingredient('French fries', 20)
+      ]),
+    new Recipe('Sushi', 'Sushi with fish and vegetables',
+      'https://www.publicdomainpictures.net/pictures/300000/nahled/sushi-items-on-a-platter.jpg', [
+        new Ingredient('Rice', 1),
+        new Ingredient('Nori', 4),
+        new Ingredient('Fish', 2)
+      ])
   ];
 
   constructor() { }
