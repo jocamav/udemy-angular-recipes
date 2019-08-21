7e3853c54c847f8ca01bce6ba05e7d21f20b08bf
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-21 12:53:56 +0200

    Save data in a server

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient) { }
+  private url = 'https://ng-recipes-course.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  saveRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.url, recipes)
+      .subscribe( responseData => {
+        console.log(responseData);
+      });
+  }
 }
