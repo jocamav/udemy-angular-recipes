5f13ef8a38afeb66858a258c6fc08f4730c03be5
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-21 14:53:37 +0200

    Add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,18 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {RecipesResolverService} from './recipes-resolver.service';
 import {HttpClientModule} from '@angular/common/http';
+import {AuthComponent} from './auth/auth.component';
 
 const appRoutes: Routes = [
   {path: '' , redirectTo: 'recipes', pathMatch: 'full'},
-  {path: 'recipes' , component: RecipesComponent, children: [
+  {path: 'recipes' , component: RecipesComponent, resolve: [RecipesResolverService], children: [
       {path: '', component: RecipeStartComponent },
       {path: 'new', component: RecipeEditComponent },
       {path: ':id', component: RecipeDetailComponent , resolve: [RecipesResolverService]},
       {path: ':id/edit', component: RecipeEditComponent , resolve: [RecipesResolverService]},
     ]},
   {path: 'shopping-list' , component: ShoppingListComponent},
+  {path: 'auth' , component: AuthComponent},
 ];
 
 @NgModule({
