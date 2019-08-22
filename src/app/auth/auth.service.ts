2e9b622ebd582ffcbc2c6cb197f33fd835bedc26
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-22 14:51:46 +0200

    Add autologout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -21,6 +21,7 @@ export interface AuthResponseData {
 export class AuthService {
 
   user = new BehaviorSubject<User>(null);
+  tokenExpirationTimer: number;
 
   private appKey = environment.googleKey;
   private url = 'https://identitytoolkit.googleapis.com/v1/accounts';
@@ -46,6 +47,8 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000));
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000 );
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private getAuthCredentials(userEmail: string, userPassword: string) {
@@ -89,8 +92,39 @@ export class AuthService {
     return throwError(message);
   }
 
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirateDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    } else {
+      const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirateDate));
+      if (loadedUser.token) {
+        this.user.next(loadedUser);
+        const expirationTime = new Date(userData._tokenExpirateDate).getTime() - new Date().getTime();
+        this.autoLogout(expirationTime);
+      }
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
   }
+
+  autoLogout(expirationDuration: number) {
+    console.log("Expiration of token: " + expirationDuration);
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
 }
