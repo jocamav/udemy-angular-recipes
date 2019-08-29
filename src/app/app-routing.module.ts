ca35363640717196a024769523854962eee9f1eb
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-29 15:06:24 +0200

    Add lazy laoding to all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthModule} from './auth/auth.module';
 
 const appRoutes: Routes = [
   {path: '' , redirectTo: 'recipes', pathMatch: 'full'},
-  {path: 'recipes' , loadChildren: './recipes/recipes.module#RecipesModule'},
+  {
+    path: 'recipes' ,
+    loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  {
+    path: 'shopping-list' ,
+    loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+  },
+  {
+    path: 'auth' ,
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
 ];
 
 @NgModule({
