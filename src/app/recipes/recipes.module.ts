a5faa570e7eedd73371a76da8b11a60581c7e924
MARTINEZ VEGA Jose Carlos (COMP-EXT) <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2019-08-29 12:24:07 +0200

    Split modules of recipes features

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,41 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RecipesComponent} from './recipes.component';
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
+import {RecipeStartComponent} from './recipe-start/recipe-start.component';
+import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
+import {AppRoutingModule} from '../app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {RecipesRoutingModule} from './recipes-routing.module';
+
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule
+  ]
+})
+export class RecipesModule {
+}
